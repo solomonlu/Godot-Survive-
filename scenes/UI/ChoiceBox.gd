b55e3ded71b90639bb33cd4ef154ca34516acd69
solomonlu <52169479@qq.com>
2024-08-03 16:57:11 +0800

    choice改成了button

--- scenes/UI/ChoiceBox.gd
+++ scenes/UI/ChoiceBox.gd
@@ -13,3 +13,23 @@ func _process(delta):
 
 func show_choice():
 	show()
+
+
+func _on_choice_text_1_pressed():
+	print("choice 1 pressed")
+	pass # Replace with function body.
+
+
+func _on_choice_text_2_pressed():
+	print("choice 2 pressed")
+	pass # Replace with function body.
+
+
+func _on_choice_text_3_pressed():
+	print("choice 3 pressed")
+	pass # Replace with function body.
+
+
+func _on_choice_text_4_pressed():
+	print("choice 4 pressed")
+	pass # Replace with function body.
