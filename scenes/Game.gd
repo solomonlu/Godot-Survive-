2e52d59c87cc79b28a09f9db4443aef00e4264ec
solomonlu <52169479@qq.com>
2024-08-16 22:39:22 +0800

    修改所有的背景图片

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -5,7 +5,8 @@ var state = STATE_IMAGE_ONLY
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var texture = load("res://textures/background/" + str(GameState.current_scene) + ".jpeg")
+	$CanvasLayer/Background.texture = texture
 
 func _input(event):
 	if event.is_action_pressed("click"):
