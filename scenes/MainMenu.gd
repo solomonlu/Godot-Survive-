d0a1e0d947d8c1e7f15d9b8791bfc8b458c8b267
Lucas <luyuchen202020@163.com>
2024-08-29 13:24:20 +0800

    添加的制作人员列表

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 func _ready():
 	$CanvasLayer2/ColorRect.hide()
+	$CanvasLayer1/Label.hide()
+	$CanvasLayer1/ButtonBackground4.hide()
 
 func change_sence(path):
 	$CanvasLayer2/ColorRect.show()
@@ -17,3 +19,17 @@ func _on_new_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_staff_pressed():
+	$CanvasLayer1/ButtonBackground.hide()
+	$CanvasLayer1/ButtonBackground3.hide()
+	$CanvasLayer1/ButtonBackground2.hide()
+	$CanvasLayer1/Label.show()
+	$CanvasLayer1/ButtonBackground4.show()
+
+func _on_quitstaff_pressed():
+	$CanvasLayer1/ButtonBackground.show()
+	$CanvasLayer1/ButtonBackground2.show()
+	$CanvasLayer1/ButtonBackground3.show()
+	$CanvasLayer1/Label.hide()
+	$CanvasLayer1/ButtonBackground4.hide()
